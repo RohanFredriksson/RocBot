2141b7a040dd8073f5f9358691fea8b36468a274
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-01-01 17:54:51 +1100

    Implemented removepool.

---
+++ commands/removepool.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { reply, capitalizeFirstLetter } = require('../util.js');
+
+const name = 'removepool';
+const description = 'Remove an existing song pool for your profile!';
+const aliases = [];
+const operatorOnly = false;
+
+module.exports = {
+
+    name: name,
+    description: description,
+    aliases: aliases,
+	operatorOnly: operatorOnly,
+
+    data:   [
+                new SlashCommandBuilder()
+                .setName(name)
+                .setDescription(description)
+                .addStringOption(option =>
+                    option.setName("pool")
+                    .setDescription("Enter the pool name of the pool you wish to remove.")
+                    .setRequired(true)
+                )
+            ],
+
+    async execute(interaction, args, client, user) {
+
+        const id = user.id;
+    
+        if (args.length < 1) {
+            reply(interaction, 'Not enough arguments!');
+            return;
+        }
+
+        pool = args[0].toLowerCase();
+
+        if (!user.hasPool(pool)) {
+            reply(interaction, 'Pool does not exist! Nothing happened.');
+            return;
+        }
+
+        user.removePool(pool);
+        user.save();
+
+        reply(interaction, 'Pool \"' + capitalizeFirstLetter(pool) + '\" removed!');
+
+	}
+
+};
